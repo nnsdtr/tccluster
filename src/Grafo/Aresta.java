e56203357118b8c515b67a4752ac0f81fd8e4b2b
Daniel T Nunes <nnsdtr.github@gmail.com>
2021-12-06 14:43:06 -0300

    Adicionando toString()

--- src/Grafo/Aresta.java
+++ src/Grafo/Aresta.java
@@ -1,8 +1,7 @@
 package Grafo;
 
-import java.util.NoSuchElementException;
-
 public class Aresta implements Comparable<Aresta> {
+    private static final int PESO_BASE = 10_000_000;
     private final int v1;
     private final int v2;
     private final int peso;
@@ -20,5 +19,11 @@ public class Aresta implements Comparable<Aresta> {
     public boolean equals(int u, int v) {
         return (this.v1 == u & this.v2 == v) ||
                 (this.v1 == v & this.v2 == u);
+    }
+
+    public String toString() {
+        if(this.v1 < this.v2)
+            return this.v1 + "-" + this.v2 + " (peso: " + this.peso + ")";
+        return this.v2 + "-" + this.v1 + " (peso: " + this.peso + ")";
     }
 }
